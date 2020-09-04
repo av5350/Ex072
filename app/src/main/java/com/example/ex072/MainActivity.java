3de88cbbd2a5fc5a4a23c6cae2c034b54789e0ae
av5350@bs.amalnet.k12.il <itayw5350>
2020-09-04 13:04:35 +0300

    start design

---
+++ app/src/main/java/com/example/ex072/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ex072;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    float aValue, bValue, cValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
