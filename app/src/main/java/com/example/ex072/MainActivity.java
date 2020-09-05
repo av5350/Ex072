7dd7ab63f4e6e1b4a66b65019f00dbbda32896cc
av5350@bs.amalnet.k12.il <itayw5350>
2020-09-05 16:41:29 +0300

    start code

--- app/src/main/java/com/example/ex072/MainActivity.java
+++ app/src/main/java/com/example/ex072/MainActivity.java
@@ -1,15 +1,81 @@
 package com.example.ex072;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    float aValue, bValue, cValue;
+    EditText aValue, bValue, cValue;
+
+    String aString, bString, cString;
+
+    Intent si;
+
+    TextView shownRoots;
+
+    Random rnd;
+
+    final int CODE_SOLVER_ACTIVITY = 4567;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aValue = (EditText)findViewById(R.id.aValue);
+        bValue = (EditText)findViewById(R.id.bValue);
+        cValue = (EditText)findViewById(R.id.cValue);
+
+        rnd = new Random();
+
+        si = new Intent(this, SolverActivity.class);
+
+        shownRoots = (TextView) findViewById(R.id.shownRoots);
+    }
+
+    public void getValues(View view) {
+        aString = aValue.getText().toString();
+        bString = bValue.getText().toString();
+        cString = cValue.getText().toString();
+
+        // if one of the cells is empty
+        if (aString.equals("") || bString.equals("") || cString.equals("") ||
+                aString.equals("-") || cString.equals("-") || bString.equals("-"))
+        {
+            Toast.makeText(this, "Enter a number in each cell!", Toast.LENGTH_SHORT).show();
+        }
+        else if (Float.parseFloat(aString) == 0){
+            Toast.makeText(this, "a cannot be equal to 0!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            si.putExtra("aValue", Double.parseDouble(aString));
+            si.putExtra("bValue", Double.parseDouble(bString));
+            si.putExtra("cValue", Double.parseDouble(cString));
+            startActivityForResult(si, CODE_SOLVER_ACTIVITY);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int source, int good, @Nullable Intent data_back) {
+        super.onActivityResult(source, good, data_back);
+        if (data_back != null) {
+            shownRoots.setText("The answers are:\n" + data_back.getStringExtra("answer"));
+        }
+    }
+
+    public void randomNums(View view) {
+        // generate random values to the a,b,c text boxs
+        // 9999999 -> the highest value that can shown in the tV without problems
+        aValue.setText("" + rnd.nextInt(9999999));
+        bValue.setText("" + rnd.nextInt(9999999));
+        cValue.setText("" + rnd.nextInt(9999999));
     }
 }
--- app/src/main/java/com/example/ex072/SolverActivity.java
+++ app/src/main/java/com/example/ex072/SolverActivity.java
@@ -2,13 +2,82 @@ package com.example.ex072;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SolverActivity extends AppCompatActivity {
+    Intent gi;
+
+    double aValue, bValue, cValue, root1, root2, discriminant;
+
+    String rootsAnswer;
+
+    TextView shownRoots;
+
+    WebView wV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solver);
+
+        shownRoots = (TextView) findViewById(R.id.shownRoots);
+
+        wV = (WebView)findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
+
+        gi = getIntent();
+        aValue = gi.getDoubleExtra("aValue", 1);
+        bValue = gi.getDoubleExtra("bValue", 1);
+        cValue = gi.getDoubleExtra("cValue", 1);
+
+        solveEquation();
+        createAnswer();
+
+        shownRoots.setText(rootsAnswer);
+        wV.loadUrl("https://www.google.com/search?q=" + aValue + "x%5E2%2B" + bValue + "*x%2B" + cValue);
+    }
+
+    public void solveEquation (){
+        // calculate the discriminant -> b^2 - 4ac
+        discriminant = Math.pow(bValue, 2) - 4 * aValue * cValue;
+
+        // calculate the roots
+        root1 = (-bValue + Math.sqrt(discriminant)) / (2 * aValue);
+        root2 = (-bValue - Math.sqrt(discriminant)) / (2 * aValue);
+    }
+
+    public void createAnswer(){
+        // there are 2 different roots
+        if (discriminant > 0){
+            rootsAnswer = "X1 = " + root1 + "\nX2 = " + root2;
+        }
+        // there is only 1 root
+        else if (discriminant == 0){
+            rootsAnswer = "X = " + root1;
+        }
+        // no real roots (there is imaginary part)
+        else {
+            rootsAnswer = "There is no solution!";
+        }
+    }
+
+    public void returnMain(View view) {
+        gi.putExtra("answer", rootsAnswer);
+        setResult(RESULT_OK, gi);
+        finish();
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
